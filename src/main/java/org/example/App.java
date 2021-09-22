1332fd8be2fdfff1af4ae9e117102a9727858305
KatsiarynaMolochko <katsyarina.molochko@gmail.com>
2021-09-23 00:32:36 +0300

    initial

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,59 +1,44 @@
 package org.example;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.lang.Math;
 import java.util.Scanner;
+import java.util.Formatter;
 import java.math.RoundingMode;
 
 
 
 class MathCalc {
-    private static final BigDecimal ZERO_D = BigDecimal.valueOf(0);
-    private static final BigDecimal ONE_D = BigDecimal.valueOf(1);
-
-    private static final BigInteger ZERO_I = BigInteger.valueOf(0);
-    private static final BigInteger ONE_I = BigInteger.valueOf(1);
-
-
-    public static BigDecimal pow(BigDecimal x, BigInteger n) {
-        BigDecimal result = ONE_D;
-
-        for (BigInteger i = ZERO_I; i.compareTo(n) < 0; i = i.add(ONE_I)) {
-            result = result.multiply(x);
+    public static double pow(double x, int n) {
+        double result = 1.0;
+        for (int i = 0; i < n; ++i) {
+            result *= x;
         }
-
         return result;
     }
 
 
-    public static BigDecimal Series(double x, int k) {
-        final BigDecimal big_x = BigDecimal.valueOf(x);
-        final BigInteger big_k = BigInteger.valueOf(k);
-        BigDecimal component = big_x;
+    public static double Series(double x, int k) {
+        double component = x;
+        double EPS = (1.0/pow(10.0, k));
+        double right = 0.0;
+        double step_d = 1.0;
+        int step_i = 1;
 
-        final BigDecimal EPS = BigDecimal.ONE.divide(
-                pow(BigDecimal.valueOf(10), big_k)
-         );
+        while (component > EPS) {
+            component = pow(x, step_i)/(step_d);
+            right -= component;
+            ++step_i;
+            ++step_d;
 
-        BigDecimal right = BigDecimal.ZERO;
-        BigInteger step = ONE_I;
-        BigDecimal step_d = ONE_D;
-
-        while (component.compareTo(EPS) > 0) {
-            component = pow(big_x, step).divide(step_d, k+1, RoundingMode.HALF_UP);
-            right = right.subtract(component);
-            step = step.add(ONE_I);
-            step_d = step_d.add(ONE_D);
         }
-
         return right;
     }
-    public static BigDecimal StandartSeries (double x, int k) {
-        final BigDecimal left = BigDecimal.valueOf(Math.log(1 - x));
 
-        return left;
+
+    public static double StandartSeries (double x) {
+        return Math.log(1 - x);
     }
+
 
 }
 public class App
@@ -69,7 +54,12 @@ public class App
         }
         System.out.println("Input k: ");
         int k = in.nextInt();
-        System.out.println("Series Calculations: " + MathCalc.Series(x, k));
-        System.out.println("Standard Calculations: " + MathCalc.StandartSeries(x, k));
+        if (k < 0) {
+            System.out.println("k should be positive:");
+            return;
+        }
+
+        System.out.println("Series Calculations: " + MathCalc.Series(x,k));
+        System.out.println("Standard Calculations: " + MathCalc.StandartSeries(x));
     }
 }
